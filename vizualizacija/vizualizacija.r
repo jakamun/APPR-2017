0e792b44c988e9a6133652006963dd48601e671b
Jaka Munda <jaka.munda@siol.net>
2017-05-22 11:48:00 +0200

    grafi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,23 @@
 # 3. faza: Vizualizacija podatkov
+library(dplyr)
+library(gsubfn)
+library(ggplot2)
+
+
+# Grafi
+
+
+
+
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
+#zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
+ #                            "OB/OB", encoding = "Windows-1250")
+#levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+ # { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+#zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
+#zemljevid <- pretvori.zemljevid(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+ # summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
